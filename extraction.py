50e7c9cd6f440bff448f32342584470f833c0615
ACHILLE DESREUMAUX <adesreum@uwaterloo.ca>
2024-10-04 12:12:46 +0200

    [FEAT] Added ArgParse to extraction script

--- extraction.py
+++ extraction.py
@@ -1,5 +1,11 @@
 import os
 import re
+import argparse
+
+parser = argparse.ArgumentParser(description="""The Best Search Engine: Research a wikipedia article, the closest within our database will be outputed.""")
+parser.add_argument("--query", metavar="query", dest="query", 
+                    required=True, action="store",
+                    help="""Write your query. It can be multiple words.""")
 
 # Fonction pour rechercher un mot dans un fichier
 def search_word_in_file(file_path, word):
@@ -20,5 +26,5 @@ def search_word_in_directory(directory, word):
 
 # Exemple d'utilisation
 directory = 'wiki_split_extract_2k' 
-word = 'le'  
-search_word_in_directory(directory, word)
+words = parser.parse_args().query
+search_word_in_directory(directory, words)
